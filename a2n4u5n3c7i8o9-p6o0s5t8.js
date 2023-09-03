b72fd1f8d273dc3d957d22892139e6b7898791d7
l3g4nd4d0 <141688303+l3g4nd4d0@users.noreply.github.com>
2023-09-03 20:55:57 -0300

    Update a2n4u5n3c7i8o9-p6o0s5t8.js

--- a2n4u5n3c7i8o9-p6o0s5t8.js
+++ a2n4u5n3c7i8o9-p6o0s5t8.js
@@ -1,8 +1,8 @@
-const anuncioPost = document.getElementById('anuncio-post');
-const getTitle = document.querySelector('.entry-title');
-const postTitle = getTitle.textContent;
+const div = document.getElementById('anuncio-post');
+const h1 = document.querySelector('.entry-title');
+const text = h1.textContent;
 
-anuncioPost.innerHTML = `
+div.innerHTML = `
 <marquee style="padding-top: 10px;margin-bottom: -20px;">
-<font color="orange" style="color: var(--box-anuncio-font);clear: both;text-align: center;margin-bottom: -5px;padding-top: 15px;text-shadow: 2px 2px 8px #ffffff;font-size: 15px;">Venha fazer parte da nossa comunidade no <a href="https://discord.gg/cGJzEDvPhM" target="_blank" style="text-transform: none;">Discord</a> e seja o primeiro a receber notificações sobre novos episódios e conteúdos exclusivos de <b>${postTitle}</b>!</font> | <span style="color: #00ceff; clear: both;text-align: center; margin-bottom: -5px; padding-top: 15px; text-shadow: 2px 0px 8px #7900ff; font-size: 15px;">Algum erro ou bug impedindo você de aproveitar o episódio? Avise-nos! Compartilhe o problema nos comentários ou no servidor do <a href="https://discord.gg/cGJzEDvPhM" target="_blank" style="text-transform: none;">Discord</a> para que possamos corrigi-lo.</span></marquee>
+<span color="orange" style="color: var(--box-anuncio-font);clear: both;text-align: center;margin-bottom: -5px;padding-top: 15px;text-shadow: 2px 2px 8px #ffffff;font-size: 15px;">Venha fazer parte da nossa comunidade no <a href="https://discord.gg/cGJzEDvPhM" target="_blank" style="text-transform: none;">Discord</a> e seja o primeiro a receber notificações sobre novos episódios e conteúdos exclusivos de <b>${text}</b>!</span> | <span style="color: #00ceff; clear: both;text-align: center; margin-bottom: -5px; padding-top: 15px; text-shadow: 2px 0px 8px #7900ff; font-size: 15px;">Algum erro ou bug impedindo você de aproveitar o episódio? Avise-nos! Compartilhe o problema nos comentários ou no servidor do <a href="https://discord.gg/cGJzEDvPhM" target="_blank" style="text-transform: none;">Discord</a> para que possamos corrigi-lo.</span></marquee>
 `;
